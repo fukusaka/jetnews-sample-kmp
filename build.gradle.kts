58b81fdaa115ab5f33eea23475099c68a8aa3a3d
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2024-04-08 00:31:45 +0900

    add kmp 1.6.1/kotlin 1.9.23

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,11 @@
  */
 
 plugins {
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.compose) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.gradle.versions)
     alias(libs.plugins.version.catalog.update)
 }
