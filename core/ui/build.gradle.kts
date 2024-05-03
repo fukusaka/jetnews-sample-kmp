19c624eb0f507e847b04703d042825280696292c
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2024-05-04 03:15:29 +0900

    core:ui の build.gradle.kts の簡素化

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,53 +1,17 @@
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.compose)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.android.library)
+    id("convention.kmp")
+    id("convention.kmp.android")
+    id("convention.kmp.compose")
+    id("convention.kmp.compose.resources")
 }
+
+android.namespace = "com.example.jetnews.core.ui"
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
-        }
-    }
-
     sourceSets {
-        androidMain {
-            dependencies {
-                implementation(libs.androidx.core.ktx)
-                implementation(libs.androidx.appcompat)
-                implementation(libs.google.android.material)
-
-                val composeBom = project.dependencies.platform(libs.androidx.compose.bom)
-                implementation(composeBom)
-                implementation(libs.androidx.compose.material3)
-                implementation(libs.androidx.compose.foundation.layout)
-                implementation(libs.androidx.compose.ui.tooling.preview)
-            }
-        }
         commonMain.dependencies {
-            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.preview)
         }
-    }
-}
-
-android {
-    namespace = "com.example.jetnews.core.ui"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        consumerProguardFiles("consumer-rules.pro")
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    dependencies {
-        testImplementation(libs.junit)
-        androidTestImplementation(libs.androidx.test.ext.junit)
-        androidTestImplementation(libs.androidx.test.espresso.core)
     }
 }
