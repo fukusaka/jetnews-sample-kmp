cafff451f828a7516b29510d9d01c828a325c492
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2024-05-04 02:17:30 +0900

    core:data の build.gradle.kts の簡素化

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,49 +1,16 @@
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.compose)
-    alias(libs.plugins.compose.compiler)
+    id("convention.kmp")
+    id("convention.kmp.android")
+    id("convention.kmp.compose")
+    id("convention.kmp.compose.resources")
 }
+
+android.namespace = "com.example.jetnews.core.data"
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
-        }
-    }
     sourceSets {
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.components.resources)
-            implementation(libs.kotlinx.collections.immutable)
+            implementation(project(":core:model"))
         }
-        androidMain {
-            dependencies {
-                implementation(project(":core:model"))
-                implementation(libs.androidx.core.ktx)
-                implementation(libs.androidx.appcompat)
-                implementation(libs.google.android.material)
-            }
-        }
-    }
-}
-
-android {
-    namespace = "com.example.jetnews.core.data"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        consumerProguardFiles("consumer-rules.pro")
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    dependencies {
-        testImplementation(libs.junit)
-        androidTestImplementation(libs.androidx.test.ext.junit)
-        androidTestImplementation(libs.androidx.test.espresso.core)
     }
 }
