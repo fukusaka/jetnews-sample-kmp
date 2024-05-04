0436f9f483361be4bb32b7e3f61e38f080e59e86
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2024-05-05 06:29:10 +0900

    feature:home の build.gradle.kts の簡素化

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -1,69 +1,20 @@
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.compose)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.android.library)
+    id("convention.feature.kmp.compose")
 }
+
+android.namespace = "com.example.jetnews.feature.home"
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
-        }
-    }
-
     sourceSets {
-        androidMain {
-            dependencies {
-                implementation(project(":core:model"))
-                implementation(project(":core:data"))
-                implementation(project(":core:ui"))
-                implementation(project(":core:designsystem"))
-
-                implementation(project(":feature:article"))
-
-                implementation(libs.androidx.core.ktx)
-                implementation(libs.androidx.appcompat)
-                implementation(libs.google.android.material)
-
-                val composeBom = project.dependencies.platform(libs.androidx.compose.bom)
-                implementation(composeBom)
-                implementation(libs.androidx.compose.material.iconsExtended)
-                implementation(libs.androidx.compose.material3)
-                implementation(libs.androidx.compose.foundation.layout)
-                implementation(libs.androidx.compose.ui.tooling.preview)
-
-                implementation(libs.accompanist.swiperefresh)
-
-                implementation(libs.androidx.lifecycle.viewmodel.ktx)
-                implementation(libs.androidx.lifecycle.runtime.compose)
-                implementation(libs.androidx.navigation.compose)
-            }
-        }
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.components.resources)
-            implementation(libs.kotlinx.collections.immutable)
-        }
-    }
-}
+            implementation(project(":feature:article"))
 
-android {
-    namespace = "com.example.jetnews.feature.home"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        consumerProguardFiles("consumer-rules.pro")
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    dependencies {
-        testImplementation(libs.junit)
-        androidTestImplementation(libs.androidx.test.ext.junit)
-        androidTestImplementation(libs.androidx.test.espresso.core)
+            implementation(libs.kmp.lifecycle.runtime.compose)
+            implementation(libs.kmp.lifecycle.viewmodel.compose)
+            implementation(libs.kmp.navigation.compose)
+        }
+        androidMain.dependencies {
+            implementation(libs.accompanist.swiperefresh)
+        }
     }
 }
